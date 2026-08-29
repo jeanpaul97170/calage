0a9e7a97ea3557f0c926108ad1805ffb02c81645
Bouchmont® Dev <jeanpaul_boucher@orange.fr>
2026-08-29 14:33:46 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    À CHAQUE PUBLICATION : incrémenter le numéro de CACHE ci-dessous.
    Sans cet incrément, les postes qui ont déjà ouvert l'outil conservent
    l'ancienne version. */
-const CACHE = "idj-controle-v106";
+const CACHE = "idj-controle-v108";
 const COQUILLE = ["./", "./index.html", "./referentiel.js", "./charte.js", "./tarifs.js",
                   "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
